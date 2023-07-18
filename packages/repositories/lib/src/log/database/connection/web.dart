17b54e6d1e15cfa84ae3e86a4fc1b03580a56c49
ThunderAnimal <martinweber.9393@gmail.com>
2023-07-19 00:24:35 +0200

    fixed absolute path for sqlite.wasm

--- packages/repositories/lib/src/log/database/connection/web.dart
+++ packages/repositories/lib/src/log/database/connection/web.dart
@@ -6,8 +6,8 @@ DatabaseConnection connect() {
   return DatabaseConnection.delayed(Future(() async {
     final db = await WasmDatabase.open(
       databaseName: 'logs_db',
-      sqlite3Uri: Uri.parse('/sqlite3.wasm'),
-      driftWorkerUri: Uri.parse('/drift_worker.js'),
+      sqlite3Uri: Uri.parse('./sqlite3.wasm'),
+      driftWorkerUri: Uri.parse('./drift_worker.js'),
     );
 
     if (db.missingFeatures.isNotEmpty) {
